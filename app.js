9ec4108e14b88a397ef671fd3cb3c707fb63e880
Nemr Aslam <you@example.com>
2021-11-22 19:06:12 +0400

    working css

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require('express');
 //creating app 
 const app = express(); 
 //send an HTTP response when receiving HTTP GET / 
+app.use(express.static('public')); 
 app.get('/', (req, res) => { 
-    res.sendFile('public/index.html', { root: __dirname });
+    res.sendFile('index.html', { root: __dirname });
 });
 //make the app listen on port const 
 port = process.argv[2] || process.env.PORT || 3000; 
